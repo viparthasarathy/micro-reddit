850aeea0c194fa63b23695a3f1c1c4bfe1a6488f
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-01-06 09:57:39 -0800

    add tests and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true, length: {maximum: 20 },
+						 uniqueness: { case_sensitive: false }
+	validates :email, presence: true 
+	validates :password, presence: true, length: {maximum: 20 }
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,26 +30,18 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "username should not be too long" do
-  	@user.name = "a" * 15
+  	@user.name = "a" * 21
   	assert_not @user.valid?
   end
 
   test "password should not be too long" do
-  	@user.password = "a" * 20
+  	@user.password = "a" * 21
   	assert_not @user.valid?
-  end
-
-  test "username should contain only alphanumericals" do
-  	invalid_names = %w[john_smith !!@$sal ada*m f00- the.king ]
-  	invalid_names.each do |invalid_name|
-  		@user.name = invalid_name
-  		assert_not @user.valid?, "#{invalid_name.inspect} should be invalid"
-  	end
   end
 
   test "username should be unique" do
   	clone_user = @user.dup
-  	close_user.email = @user.name
+  	clone_user.email = @user.name.upcase
   	@user.save
   	assert_not clone_user.valid?
   end
